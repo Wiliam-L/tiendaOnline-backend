acd4253a0ed3d8b73236a4ee62b0799d7e7febd1
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-16 00:44:43 -0600

    feat: Agregar archivo index.js con configuración de servidor Express y endpoints para productos, categorías, usuarios, roles, estados, órdenes, detalles de órdenes y clientes

---
+++ index.js
@@ -0,0 +1,94 @@
+import dotenv from "dotenv";
+import express from "express";
+const app = express();
+dotenv.config();
+
+//Middleware para parsear JSON
+app.use(express.json());
+
+//usar puerto desde .env o uno por defecto
+const PORT = process.env.PORT || 3000;
+
+//Enpoints productos
+app.post("/create/products/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/products/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para categorias
+app.post("/create/categories/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/categories/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para usuarios
+app.post("/create/users/", (req, res) => {
+  const data = req.body;
+});
+
+//Enpoint para rol
+app.post("/create/rol/", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+app.post("/update/rol/:id", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/users/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para estados
+app.post("/create/states/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/states/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para Orden
+app.post("/create/orders/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/orders/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para ordenDetalle
+app.post("/create/order-details/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/order-details/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+//Enpoints para clientes
+app.post("/create/clients/", (req, res) => {
+  const data = req.body;
+});
+
+app.patch("/update/clients/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutandose en el Puerto ${PORT}`);
+});
