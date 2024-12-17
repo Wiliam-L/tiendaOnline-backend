24377d13c0e1d081bf0ef5254ef0fb1cea1024e4
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-17 00:33:05 -0600

    feat: agregar 'registerController' para manejar la creación de clientes y usuarios en un único endpoint.

---
+++ app/controller/registerController.js
@@ -0,0 +1,45 @@
+import { createClient } from "../services/clientService.js";
+import { createUser } from "../services/userService.js";
+import sequelize from "../config/db.js";
+
+export const registerController = async (req, res) => {
+  const { cliente, usuario } = req.body;
+
+  try {
+    await sequelize.transaction(async (t) => {
+      //1. Crear cliente
+      const clienteResult = await createClient(
+        cliente.razon_social,
+        cliente.nombre_comercial,
+        cliente.direccion_entrega,
+        cliente.telefono,
+        cliente.correo_electronico,
+        { transaction: t }
+      );
+
+      //2. Crear usuario
+      const userResult = await createUser(
+        usuario.rol_id,
+        usuario.correo,
+        usuario.nombre,
+        usuario.password,
+        usuario.telefono,
+        usuario.fechaNacimiento,
+        { transaction: t }
+      );
+    });
+
+    // Si todo salió bien
+    return res.status(201).json({
+      message: "Usuario registrado correctamente",
+    });
+  } catch (error) {
+    if (error.message === "El correo electrónico ya está registrado") {
+      return res.status(400).json({ message: error.message });
+    }
+    return res.status(500).json({
+      message: "Hubo un error al crear el cliente",
+      error: error.message,
+    });
+  }
+};
