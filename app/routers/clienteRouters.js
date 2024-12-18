e484f37f61acbbc3d9dbbea925a9e691210bfd90
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-18 01:32:42 -0600

    Revert "feat: agregar ruta para crear cliente en clienteRouters"
    This reverts commit 6a7ca99cc75ade4c3b194fba46f8b52b2a83bd24.

--- app/routers/clienteRouters.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { crearClienteController } from "../controller/createClienteController.js";
-
-const router = express.Router();
-
-router.post("/create-client", crearClienteController);
-
-export default router;
