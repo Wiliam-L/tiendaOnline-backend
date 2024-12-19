bdd5c9afb9787730a02d2f2e7d33f3c4928cbabd
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-19 01:01:46 -0600

    feat: agregar middleware checkRole en productRouter para controlar el acceso

--- app/routers/productRoutes.js
+++ app/routers/productRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import getFile from "../utils/multer.js";
 import createProducts from "../controller/productsController.js";
+import { checkRole } from "../utils/token.js";
 
 const router = Router();
 
-router.post("/create/product", getFile.single("foto"), createProducts);
+router.post(
+  "/create/product",
+  checkRole(["administrador", "gerente"]),
+  getFile.single("foto"),
+  createProducts
+);
 
 export default router;
