3882d64cd0fd8d20101d82e4f2256b241d99b2cc
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-19 00:57:13 -0600

    refactor: eliminar id_producto y id_usuario, utilizar procedimiento almacenado createProducts para crear el producto

--- app/controller/productsController.js
+++ app/controller/productsController.js
@@ -1,26 +1,17 @@
 import sequelize from "../config/db.js";
 import { createProducts } from "../services/productsService.js";
+import { getIdUserForCookie } from "../utils/token.js";
 
 const MAX_SIZE = 10 * 1024 * 1024; //10MB
 
 const createProductsController = async (req, res) => {
-  const {
-    id_producto,
-    id_categoria,
-    id_usuario,
-    nombre,
-    marca,
-    codigo,
-    stock,
-    id_estado,
-    precio,
-  } = req.body;
+  const { id_categoria, nombre, marca, codigo, stock, id_estado, precio } =
+    req.body;
 
   //verificar si se cargo la foto
   const foto = req.file;
 
   if (!foto) {
-    console.log(req.files);
     return res.status(400).json({ error: "No se ha cargado ninguna foto" });
   }
 
@@ -34,9 +25,54 @@ const createProductsController = async (req, res) => {
     return res.status(400).json({ error: "El archivo es demasiado grande" });
   }
 
-  //Obtener binario de la imagen
+  //obtener binario de la imagen
   const ft_binary = foto.buffer;
-  console.log(typeof ft_binary);
+
+  //obtener id del usuario
+  const id_usuario = getIdUserForCookie(req);
+
+  try {
+    await createProducts(
+      parseInt(id_categoria),
+      parseInt(id_usuario),
+      nombre,
+      marca,
+      codigo,
+      parseFloat(stock),
+      parseInt(id_estado),
+      parseFloat(precio),
+      ft_binary
+    );
+
+    return res.status(201).json({ message: "Producto creado con éxito" });
+  } catch (error) {
+    if (error.name === "SequelizeForeignKeyConstraintError") {
+      return res.status(400).json({ message: "El código no existe" });
+    }
+
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .json({ message: "El código del producto ya en uso" });
+    }
+
+    if (
+      error.message === "Faltan campos oblogatorios para crear el producto."
+    ) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    if (error.message === "Precio no válido.") {
+      return res.status(400).json({ message: error.message });
+    }
+
+    if (error.message === "Código ya esta en uso") {
+      return res.status(400).json({ message: error.message });
+    }
+    if (error.message === "La cantidad para el stock no es válido.") {
+      return res.status(400).json({ message: error.message });
+    }
+  }
 };
 
 export default createProductsController;
