09d0e04f4cad86fa03c74df9591bead2863aeb94
Wiliam López <wlopezguerra2000@gmail.com>
2024-12-19 01:31:16 -0600

    feat: - agregar lógica para generar tokens de acceso y refresco - actualizar token de acceso - validar token de acceso - verificar el rol del usuario - obtener el id del usuario desde la cookie - eliminar archivo usuarioRouters.js

--- app/routers/usuarioRouters.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createUserController } from "../controller/createUserController.js";
-
-const router = express.Router();
-
-router.post("/register", createUserController);
-export default router;
